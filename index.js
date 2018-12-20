773bfc77bfe9ccf31b97d1ac02801e44b31ff8ab
jstricklin89 <jstricklin89@gmail.com>
2018-12-20 09:44:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+  let minIndex = 0;
+  let minValue = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return minValue;
 }
 
-function insertionSort(array){
-
+function insertionSort(array) {
+  let newArray = [];
+  while (array.length != 0) {
+    let value = findMinAndRemove(array);
+    newArray.push(value);
+  }
+  return newArray;
 }
